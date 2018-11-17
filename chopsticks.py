093604dcf6d575c05f8fe9b2c7399c569314afa6
travis millott <travis.millott@gmail.com>
2018-11-16 23:57:33 -0700

    add chopsticks game

---
+++ chopsticks.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+PLAYER_1 = 'p1'
+PLAYER_2 = 'p2'
+SPLIT = 's'
+POSSIBLE_MOVES = ['01','00','10','11',SPLIT]
+HELP_TEXT = """
+Moving examples- 
+  Using my 0 index hand, hit other player's 1 index hand: 01
+  Using my 1 index hand, hit other player's 1 index hand: 11
+  Split: s
+"""
+
+INVALID_MOVE                  = 'Invalid move, possible moves are: ' + str(POSSIBLE_MOVES)
+INVALID_CANNOT_SPLIT          = 'Bad move. Cannot split.'
+INVALID_NO_ROLLOVER           = 'Bad move.  Cannot add past 5'
+INVALID_NO_FINGERS_BEING_USED = 'Bad move.  Hand must have some fingers to be used'
+
+class Player:
+  def __init__(self):
+    self.hands = [1,1]
+
+  def canSplit(self):
+    return self.aHandIsEven() and self.aHandIsEmpty()
+
+  def aHandIsEven(self):
+    return (self.hands[0] % 2 == 0 and self.hands[0] != 0) \
+        or (self.hands[1] % 2 == 0 and self.hands[1] != 0)
+
+  def aHandIsEmpty(self):
+    return self.hands[0] == 0 or self.hands[1] == 0
+
+
+class ChopsticksGame:
+
+  def __init__(self):
+    self.players = {PLAYER_1 : Player(), PLAYER_2 : Player()}
+    self.playerToMove = PLAYER_1
+    self.gameStateHistory = []
+
+  def printGameState(self):
+    print('p1:' + str(self.players[PLAYER_1].hands) + '  p2:' + str(self.players[PLAYER_2].hands) + '  Player to move: ' + self.playerToMove)
+
+
+  def opponent(self, player):
+    if player == PLAYER_1:
+      return PLAYER_2
+    return PLAYER_1
+
+  def checkIfMoveIsValid(self, move):
+    if move not in POSSIBLE_MOVES:
+      return INVALID_MOVE
+    player = self.players[self.playerToMove]
+    opponentPlayer = self.players[self.opponent(self.playerToMove)]
+    if move == SPLIT and not player.canSplit() :
+      return INVALID_CANNOT_SPLIT
+    if move != SPLIT: 
+      playerHand = int(move[0])
+      opponentHand = int(move[1])
+      if player.hands[playerHand] + opponentPlayer.hands[opponentHand] > 5:
+        return INVALID_NO_ROLLOVER
+      if player.hands[playerHand] == 0 or opponentPlayer.hands[opponentHand] == 0:
+        return INVALID_NO_FINGERS_BEING_USED
+
+  def inputMove(self):
+    #Swap this function out with AI or use it to build tree - but be sure to use validation 
+    while True:
+      move = input('(' + self.playerToMove + ') : ')
+      print(str(move))
+      validationString = self.checkIfMoveIsValid(move)
+      if validationString:
+        #Error with move, print error and ask for move again
+        print(validationString)
+        continue
+      else:
+        break
+    return move
+
+
+  def doMove(self, move):
+    if move == SPLIT:
+      fromHandIndex = 0 if self.players[self.playerToMove].hands[1] == 0 else 1
+      splitAmount = int(self.players[self.playerToMove].hands[fromHandIndex] / 2)
+      self.players[self.playerToMove].hands[0] = splitAmount
+      self.players[self.playerToMove].hands[1] = splitAmount
+    else:
+      playerHand = int(move[0])
+      opponentHand = int(move[1])
+      self.players[self.opponent(self.playerToMove)].hands[opponentHand] += self.players[self.playerToMove].hands[playerHand]
+      if self.players[self.opponent(self.playerToMove)].hands[opponentHand] == 5:
+        self.players[self.opponent(self.playerToMove)].hands[opponentHand] = 0
+
+  def switchPlayerToMove(self):
+    self.playerToMove = self.opponent(self.playerToMove)
+
+  def situationalSuperKoViolated(self):
+    return self.gameState() in self.gameStateHistory
+
+  def checkForWinningPlayer(self):
+    if self.situationalSuperKoViolated():
+      return self.playerToMove
+    #TODO: Doesn't work, write unit tests for this
+    if not self.playerHasValidMove():
+      return self.opponent(self.playerToMove)
+    for player in [PLAYER_1, PLAYER_2]:
+      hands = self.players[player].hands
+      if hands[0] == 0 and hands[1] == 0:
+        return self.opponent(player)
+
+  def gameState(self):
+    return [''.join([str(self.players[PLAYER_1].hands), str(self.players[PLAYER_2].hands), self.playerToMove])]
+
+  def playerHasValidMove(self):
+    for move in POSSIBLE_MOVES:
+      if self.checkIfMoveIsValid(move):
+        return True
+    return False
+
+
+  def saveGameStateHistory(self):
+    self.gameStateHistory += self.gameState()
+    #print(self.gameStateHistory)
+
+  def doTurn(self, move):
+    # Returns the winning player if the game has ended
+    self.saveGameStateHistory()
+    self.doMove(move)
+    self.switchPlayerToMove()
+
+    winningPlayer = self.checkForWinningPlayer()
+    if winningPlayer:
+      print('Winning player: ' + winningPlayer)
+      return winningPlayer
+    return False
+
+
+  def play(self):
+    print(HELP_TEXT)
+    while True:
+      self.printGameState()
+      move = self.inputMove()
+      winResult = self.doTurn(move)
+      if winResult:
+        break
+
+
+if __name__ == '__main__':
+  ChopsticksGame().play()
---
+++ unittest_chopsticks.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import unittest
+
+from chopsticks import *
+
+
+class TestChopsticks(unittest.TestCase):
+
+  def setUp(self):
+    self.game = ChopsticksGame()
+
+
+  def testBasicGame(self):
+    self.game.players[PLAYER_1].hands = [0,1]
+    self.game.players[PLAYER_2].hands = [0,4]
+    self.game.playerToMove = PLAYER_1
+    self.game.doTurn('11')
+    winningPlayer = self.game.checkForWinningPlayer()
+    self.assertEqual(winningPlayer, PLAYER_1)
+
+
+  def testValidateRolloverMove(self):
+    self.game.players[PLAYER_1].hands = [4,0]
+    self.game.players[PLAYER_2].hands = [4,0]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('00')
+    self.assertEqual(INVALID_NO_ROLLOVER, validationResponse)
+
+
+  def testValidateInvalidMove(self):
+    self.game.players[PLAYER_1].hands = [4,0]
+    self.game.players[PLAYER_2].hands = [4,0]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('bbq')
+    self.assertEqual(INVALID_MOVE, validationResponse)
+
+
+  def testValidateCannotSplit1(self):
+    self.game.players[PLAYER_1].hands = [3,0]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('s')
+    self.assertEqual(INVALID_CANNOT_SPLIT, validationResponse)
+
+
+  def testValidateCannotSplit2(self):
+    self.game.players[PLAYER_1].hands = [4,2]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('s')
+    self.assertEqual(INVALID_CANNOT_SPLIT, validationResponse)
+
+
+  def testValidateHittingWithEmptyHand(self):
+    self.game.players[PLAYER_1].hands = [0,2]
+    self.game.players[PLAYER_2].hands = [1,1]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('00')
+    self.assertEqual(INVALID_NO_FINGERS_BEING_USED, validationResponse)
+
+
+  def testValidateHittingAnEmptyHand(self):
+    self.game.players[PLAYER_1].hands = [1,1]
+    self.game.players[PLAYER_2].hands = [0,1]
+    self.game.playerToMove = PLAYER_1
+    validationResponse = self.game.checkIfMoveIsValid('00')
+    self.assertEqual(INVALID_NO_FINGERS_BEING_USED, validationResponse)
+
+
+  def testSplit(self):
+    self.game.players[PLAYER_1].hands = [4,0]
+    self.game.players[PLAYER_2].hands = [1,1]
+    self.game.playerToMove = PLAYER_1
+    self.game.doTurn('s')
+    self.assertEqual(self.game.players[PLAYER_1].hands, [2,2])
+
+
+def suite():
+  runSuite = unittest.TestSuite()
+  for _name,_class in globals().items():
+    if _name.startswith('Test'):
+      runSuite.addTest(unittest.makeSuite(_class))
+  return runSuite
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) > 1:
+    unittest.main()
+  else:
+    unittest.TextTestRunner().run(suite())
